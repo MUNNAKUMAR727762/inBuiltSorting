f1c13693927604dce826bfdfe5e94ebfa15beed5
MUNNAKUMAR727762 <98367773+MUNNAKUMAR727762@users.noreply.github.com>
2023-06-07 21:47:04 +0530

    Add files via upload

---
+++ inbuiltSortingMethod.java
@@ -0,0 +1,17 @@
+package star;
+import java.util.Arrays; 
+
+public class inbuiltSortingMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {5,4,1,7,8,3,2};
+		
+         Arrays.sort(arr);
+         for(int i=0; i<arr.length; i++) {
+        	 System.out.print(arr[i]+" ");
+         }
+	}
+
+}
